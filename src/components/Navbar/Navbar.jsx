2e343ce8b95b090cc0c8ed5b3652d4a06b403927
mateo chancalay <97802809+mchancalay@users.noreply.github.com>
2024-03-31 10:48:55 -0300

    navbar height

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import logo from '../../assets/logo.png'
 import telegram from '../../assets/telegram.png'
 import x from '../../assets/x.png'
@@ -10,31 +10,49 @@ import { xLink, birdeyeLink, telegramLink } from '../../js/vars';
 
 const Navbar = props => {
 
+  const [isScrolling, setIsScrolling] = useState(false);
+  
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolling(true);
+      } else {
+        setIsScrolling(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-      <div className={`nb`}>
-        <Container maxWidth="xl">
-          <div className='nb__container'>
-            <div className='nb__left'>
-              <img className='nb__logo' src={logo} alt="" />
-              <div className='nb__items'>
-                <a className='nb__item'>Home</a>
-                <a className='nb__item'>About us</a>
-                <a className='nb__item'>Road map</a>
-                <a className='nb__item'>Tokenomics</a>
-                <a className='nb__item'>Community</a>
-              </div>
-            </div>
-            <div className='nb__right'>
-              <div className='nb__links'>
-                <NavbarLink surce={x} name={'x.com'} link={xLink} />
-                <NavbarLink surce={telegram} name={'Telegram'} link={telegramLink} />
-                <NavbarLink surce={birdeye} name={'Birdeye'} link={birdeyeLink} />
-              </div>
-              <ButtonImage />
+    <div className={`nb ${isScrolling ? 'nb--scrolling' : ''}`}>
+      <Container maxWidth="xl">
+        <div className='nb__container'>
+          <div className='nb__left'>
+            <img className='nb__logo' src={logo} alt="" />
+            <div className='nb__items'>
+              <a className='nb__item'>Home</a>
+              <a className='nb__item'>About us</a>
+              <a className='nb__item'>Road map</a>
+              <a className='nb__item'>Tokenomics</a>
+              <a className='nb__item'>Community</a>
             </div>
           </div>
-        </Container>
-      </div>
+          <div className='nb__right'>
+            <div className='nb__links'>
+              <NavbarLink surce={x} name={'x.com'} link={xLink} />
+              <NavbarLink surce={telegram} name={'Telegram'} link={telegramLink} />
+              <NavbarLink surce={birdeye} name={'Birdeye'} link={birdeyeLink} />
+            </div>
+            <ButtonImage />
+          </div>
+        </div>
+      </Container>
+    </div>
   )
 }
 
