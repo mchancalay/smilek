13435e342b98673fe2b5c47642092b9862e89a8a
mateo chancalay <97802809+mchancalay@users.noreply.github.com>
2024-05-05 13:04:37 -0300

    optimizacion de imagenes y nuevos ytbers

--- src/App.jsx
+++ src/App.jsx
@@ -23,11 +23,3 @@ const App = () => {
 }
 
 export default App
-
-{
-  /*
-        <div id="dexscreener-embed" style={{ position: 'relative', width: '100%', paddingBottom: '125%' }}>
-      <iframe src="https://dexscreener.com/solana/DwwE6q9rPHKEoooMjv99hVUvA4CPUZJhws4CqrrBD7XT?embed=1&theme=dark" style={{ position: 'absolute', width: '100%', height: '100%', top: '0', left: '0', border: '0' }}></iframe>
-    </div>
-  */
-}
