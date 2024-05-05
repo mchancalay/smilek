ab6260b2fa4e2492cd0f8fa4e5e8614acf2b0828
mateo chancalay <97802809+mchancalay@users.noreply.github.com>
2024-05-05 13:08:20 -0300

    sacado de efecto

--- src/components/HeaderContent/HeaderContent.jsx
+++ src/components/HeaderContent/HeaderContent.jsx
@@ -10,7 +10,6 @@ import redirectWhite from '../../assets/redirectWhite.png'
 import { whitepaperLink, buyLink } from '../../js/vars';
 import ContractCopy from '../ContractCopy/ContractCopy'
 import Bar from '../Bar/Bar'
-import 'animate.css'
 
 const HeaderContent = () => {
 
@@ -25,7 +24,7 @@ const HeaderContent = () => {
                             <h2 className='hC__subtitle'>IT'S NEVER BEEN EASIER TO SMILE TO THE BANKS!</h2>
                         </div>
                         <div className='hC__principal'>
-                            <h1 className='hC__title animate__animated animate__bounceIn'>$SMILEK</h1>
+                            <h1 className='hC__title'>$SMILEK</h1>
                         </div>
                         <div className='hC__btns'>
                             <ButtonDefault
@@ -44,8 +43,8 @@ const HeaderContent = () => {
                         <ContractCopy />
                     </div>
                     <div className='hC__box-2'>
-                        <img src={character} alt="" className='hC__img animate__animated animate__slideInRight' />
-                        <div className='hC__heads animate__animated animate__zoomIn'>
+                        <img src={character} alt="" className='hC__img' />
+                        <div className='hC__heads'>
                             <img src={head} alt="" className='hC__head hC__head1' />
                             <img src={head} alt="" className='hC__head hC__head2' />
                             <img src={head} alt="" className='hC__head hC__head3' />
