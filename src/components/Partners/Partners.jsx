7002f830d6aec5d9ec80fd21a94cef3a57604830
mateo chancalay <97802809+mchancalay@users.noreply.github.com>
2024-10-11 14:26:41 -0300

    fix biconomy/gotbit

--- src/components/Partners/Partners.jsx
+++ src/components/Partners/Partners.jsx
@@ -4,7 +4,6 @@ import { Container, Grid } from '@mui/material';
 import coinmarketcap from '../../assets/coinmarketcap.png';
 import coingecko from '../../assets/coingecko.png';
 import mexc from '../../assets/mexc.png';
-import gotbit from '../../assets/gotbit.png';
 import tapbit from '../../assets/tapbit.png';
 import biconomy from '../../assets/biconomy.png';
 import Partner from '../Partner/Partner';
@@ -28,10 +27,10 @@ const Partners = () => {
               <Partner img={mexc} title={'MEXC'} redirect={mexcLink} />
             </Grid>
             <Grid item xs={6} sm={4} md={4} lg={3} xl={3}>
-              <Partner img={gotbit} title={'Gotbit'} redirect={gotbitLink} />
+              <Partner img={tapbit} title={'Tapbit'} redirect={tapbitLink} />
             </Grid>
             <Grid item xs={6} sm={4} md={4} lg={3} xl={3}>
-              <Partner img={tapbit} title={'Tapbit'} redirect={tapbitLink} />
+              <Partner img={biconomy} title={'Biconomy'} redirect={biconomyLink} />
             </Grid>
           </Grid>
         </div>
